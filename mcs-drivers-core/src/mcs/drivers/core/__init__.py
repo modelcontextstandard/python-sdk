5f430ee184b67e753cf59956dde0e057128cc383
Danny Gerst <d.gerst@bizrock.de>
2025-07-05 19:13:18 +0200

    Added the new interface MCSToolDriver that should be used with an orchestrator together. Renamed some classes to be more precise.

--- mcs-drivers-core/src/mcs/drivers/core/__init__.py
+++ mcs-drivers-core/src/mcs/drivers/core/__init__.py
@@ -1,3 +1,5 @@
-from .mcs_driver_interface import MCSDriver, DriverMeta
+from .mcs_driver_interface import MCSDriver, DriverMeta, DriverBinding
+from .mcs_tool_driver_interface import Tool, ToolParameter, MCSToolDriver
+from .mcs_base_orchestrator import BasicOrchestrator
 
-__all__ = ["MCSDriver", "DriverMeta"]
+__all__ = ["MCSDriver", "DriverMeta", "DriverBinding", "Tool", "ToolParameter", "MCSToolDriver", "BasicOrchestrator"]
